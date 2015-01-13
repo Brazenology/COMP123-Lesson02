52cb79a7fa2e0a9cc9fea7954114d03e954f21a2
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-01-13 13:13:39 -0500

    Added ability scores and refactored method

--- COMP123-Lesson02/Program.cs
+++ COMP123-Lesson02/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace COMP123_Lesson02
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+            int strength = 50;
+            int stamina = 10;
+            int speed = 5;
+            int toughness = 12;
+            int intelligence = 2;
+
+            Console.WriteLine("Strength in main: {0}", strength);
+
+
+            increaseAbility(strength);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
+
+
+        // method to increase an ability by 2;
+    private static void increaseAbility(int ability) {
+        ability +=2;
+        Console.WriteLine("ability in increaseAbility: {0}", ability);
     }
+
+
+
+    }
+
+   
 }
