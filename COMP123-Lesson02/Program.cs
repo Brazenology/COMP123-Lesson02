8aa512442c94d8abe199909eed879d19f210576f
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-01-13 13:34:48 -0500

    Added new method setAbility

--- COMP123-Lesson02/Program.cs
+++ COMP123-Lesson02/Program.cs
@@ -8,26 +8,38 @@ namespace COMP123_Lesson02
 
         static void Main()
         {
-            int strength = 50;
+            int strength;
             int stamina = 10;
             int speed = 5;
             int toughness = 12;
             int intelligence = 2;
 
-            Console.WriteLine("Strength in main: {0}", strength);
+            setAbility(out strength, 50);
 
 
-            increaseAbility(strength);
+            Console.WriteLine("Strength in main before: {0}", strength);
+
+
+            increaseAbility( ref strength, "Strength");
+
+
+            Console.WriteLine("Strength in main after: {0}", strength);
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
 
 
+        // Takes two parameters and sets the value of the ability that is passed
+        private static void setAbility(out int abilityName, int abilityValue) {
+            abilityName = abilityValue;
+        }
+
+
         // method to increase an ability by 2;
-    private static void increaseAbility(int ability) {
-        ability +=2;
-        Console.WriteLine("ability in increaseAbility: {0}", ability);
+    private static void increaseAbility(ref int abilityScore, String abilityName) {
+        abilityScore +=2;
+        Console.WriteLine("{0} in increaseAbility: {1}", abilityName, abilityScore);
     }
 
 
